43d7442d1771312eedac13d417ece984a8963f2f
unknown <a_d_i10@wp.pl>
2018-06-15 19:39:13 +0200

    Added background to the menu

--- src/menu/MenuWindowBackgroundGraphics.java
+++ src/menu/MenuWindowBackgroundGraphics.java
@@ -1,15 +1,27 @@
 package menu;
 
-import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 public class MenuWindowBackgroundGraphics extends JPanel {
-    
+
+    private BufferedImage img;
+    private BufferedImage img2;
+   
+    public MenuWindowBackgroundGraphics() throws URISyntaxException, IOException {       
+        img = ImageIO.read(new File("images/tlo.png"));
+        img2 = ImageIO.read(new File("images/text2029.png"));
+    }
+
     public void paint(Graphics g) {
-        g.setColor(Color.white);
-        g.fillRect(0, 0, 360, 640);
-        
+        g.drawImage(img, 0, 0, 360, 640, null, null);
+        g.drawImage(img2, 30, 100, 300, 100, null, null);
     }
 }
