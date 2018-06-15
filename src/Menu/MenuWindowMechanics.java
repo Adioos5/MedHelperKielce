cf23a0ceda6bfd4f30d91bd47da11d5bad2f3419
unknown <a_d_i10@wp.pl>
2018-06-15 14:38:25 +0200

    Added menu

---
+++ src/Menu/MenuWindowMechanics.java
@@ -0,0 +1,39 @@
+package Menu;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+import javax.swing.JButton;
+
+import javax.swing.JButton;
+
+public class MenuWindowMechanics implements ActionListener {
+
+    private JButton firstHelp;
+    private JButton healthMap;
+    private JButton settings;
+
+    public MenuWindowMechanics(JButton firstHelp, JButton healthMap, JButton settings) {
+        this.firstHelp = firstHelp;
+        this.healthMap = healthMap;
+        this.settings = settings;
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == firstHelp) {
+
+        }
+        if (e.getSource() == healthMap) {
+
+        }
+        if (e.getSource() == settings) {
+
+        }
+        
+    }
+
+}
