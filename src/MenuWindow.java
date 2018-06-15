48c794d3b7ed8b40a43f3e60520e8a6e9b5861b6
unknown <a_d_i10@wp.pl>
2018-06-15 16:16:01 +0200

    Added a way how to add a background to the button

--- src/MenuWindow.java
+++ src/MenuWindow.java
@@ -1,7 +1,9 @@
 
-import java.awt.Color;
-import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.net.URL;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
@@ -10,20 +12,26 @@ public class MenuWindow extends JFrame{
     private JButton firstHelp;
     private JButton healthMap;
     private JButton settings;
+    private URL url;
+    BufferedImage img;
     
     public MenuWindow() {
+            
+        ImageIcon img = new ImageIcon("images/mazeBackground.jpg");
+       
         
         setBounds(500,100,400,550);
         setTitle("Med Helper Kielce");
         
         firstHelp = new JButton("Pierwsza pomoc");
-        healthMap = new JButton("");
+        healthMap = new JButton("Mapa zdrowia");
        
         
         healthMap.setBounds(50,200,300,100);
         firstHelp.setBounds(50,350,300,100);
       
-        healthMap.setBackground(Color.CYAN);
+        healthMap.setIcon(img);
+        firstHelp.setIcon(img);
         
         firstHelp.addActionListener(new MenuWindowMechanics(firstHelp,healthMap,settings));
         healthMap.addActionListener(new MenuWindowMechanics(firstHelp,healthMap,settings));
