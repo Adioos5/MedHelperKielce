fc7a93d2070426f47f80281f073acde40bf7b5cf
unknown <a_d_i10@wp.pl>
2018-06-15 20:09:35 +0200

    commit

--- src/healthMap/HealthMapWindow.java
+++ src/healthMap/HealthMapWindow.java
@@ -1,30 +1,30 @@
 package healthMap;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import menu.MenuWindow;
+import menu.MenuWindowBackgroundGraphics;
 
-public class HealthMapWindow extends JFrame{
+public class HealthMapWindow extends JFrame {
 
-    private JButton firstAid;
-    private JButton healthMap;
-    
-   
-    private JPanel mainPanel;
-    private JPanel menuPanel;
-    
-    public HealthMapWindow(JPanel panel) {
-        
-        mainPanel = panel;
-        mainPanel = menuPanel;
-        
+    JPanel panel;
+
+    public HealthMapWindow() throws URISyntaxException, IOException {
+
+        panel = new MenuWindowBackgroundGraphics();
+        setBounds(500, 50, 360, 640);
+        setTitle("Med Helper Kielce");
         setResizable(false);
+        add(panel);
     }
+
     public void run() {
-        
-      
-        
+        setVisible(true);
+
     }
 }
--- src/menu/MenuWindowMechanics.java
+++ src/menu/MenuWindowMechanics.java
@@ -1,11 +1,11 @@
 package menu;
 
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 import javax.swing.JButton;
-import javax.swing.JPanel;
 
 import healthMap.HealthMapWindow;
 
@@ -13,29 +13,37 @@ public class MenuWindowMechanics implements ActionListener {
 
     private JButton firstAid;
     private JButton healthMap;
- 
 
     public MenuWindowMechanics(JButton firstAid, JButton healthMap) {
         this.firstAid = firstAid;
         this.healthMap = healthMap;
-  
 
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        
+
         if (e.getSource() == healthMap) {
-        System.out.println("Mapa zdrowia");    
+            System.out.println("Mapa zdrowia");
+            HealthMapWindow hmw = null;
+            try {
+                hmw = new HealthMapWindow();
+            } catch (URISyntaxException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            } catch (IOException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
+            
+            hmw.run();
             
         }
-        
+
         if (e.getSource() == firstAid) {
             System.out.println("Otworzenie pierwszej pomocy");
         }
-        
-        
-        
+
     }
 
 }
